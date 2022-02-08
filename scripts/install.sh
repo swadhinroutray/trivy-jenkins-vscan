a686deb3ae9a00a06272304adf1b9ec7082c8e49
swadhinroutray <swadhin.routray@gmail.com>
2022-02-08 18:46:56 +0530

    fixing folder managements

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 
 echo "Starting clean install"
 
+cd .. 
+
 mvn clean install 
 
 cp -r target/docker-java-sample-webapp-1.0-SNAPSHOT.war src/main/docker/
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,7 @@ echo "Starting Container on port 8080"
 
 # docker container run -p 5000:5000 trivy-scan:1
 
+cd ../src/main/docker 
+
 docker-compose up 
  
